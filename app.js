af2508204ec8cd2016cd9068a739c2f0f42ce64c
Cynox336 <cynox336@gmail.com>
2023-05-11 11:05:11 +0200

    Update 11/05 11:05

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 const image = document.querySelector('img');
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
+button.addEventListener("click", function() {
+    fetch("https://api.chucknorris.io/jokes/random")
+    .then(response => response.json())
+    .then(data => {
+        jokeDIV.textContent = data.value;
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+})
